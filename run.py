a6e9ba34e08324d823897f4fc0c5682ad1fa7372
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-09-27 13:21:50 +0000

    Add function to collect lesson data

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-Below settings required to link into google sheets spreadsheet
+Below code required to link into google sheets spreadsheet
 """
 import gspread
 from google.oauth2.service_account import Credentials
@@ -15,7 +15,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('yoga _flow_class_record')
 
-attendance = SHEET.worksheet('attendance')
-data = attendance.get_all_values()
+def get_lesson_data():
+    """
+    Collects lesson data from the user
+    """
+    print("Please provide lesson data from todays classes.")
+    print("The data should contain the day, date, time, duration in mins, location and attendance separated by commas")
+    print("Example: Monday,27/09/2021,08:00,60,Camden Town,15\n")
 
-print(data)
+    lesson_data_str = input("Enter your data here: ")
+    print(f"The data provided is {lesson_data_str}")
+
+get_lesson_data()
