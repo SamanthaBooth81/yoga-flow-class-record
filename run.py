f67ef3c2a180701dced455e4535fd4f895edd7a7
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-09-28 21:01:43 +0000

    add validation code to validay first input - day. Still not correct, tbc

--- run.py
+++ run.py
@@ -8,36 +8,26 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('yoga _flow_class_record')
 
-# def get_lesson_data():
-#     """
-#     Collects lesson data from the user
-#     """
-#     print("Please provide lesson data from todays classes.")
-#     print("The data should contain the day, date, time, duration in mins, location and attendance separated by commas")
-#     print("Example: Monday,27/09/2021,08:00,60,Camden Town,15\n")
-
-#     lesson_data_str = input("Enter your data here: ")
-#     print(f"The data provided is {lesson_data_str}")
-
-# get_lesson_data()
 
 def get_lesson_data():
     """
-    Collects lesson data from the user, 
-    each item pf data will need to be validated differently 
-    which is why I've created separate inputs
+    Collects lesson data from the user. 
+    Each item of data will need to be validated differendly. 
+    Therefore I have created separate inputs.
     """
     print("Please provide day of your lesson.")
     print("This should be between Monday to Sunday only\n")
     day_data_str = input("Enter your data here: ")
     print("\n")
+
+    validate_data(day_data_str)
 
     print("Please provide date of your lesson.")
     print("This should be in the format DD/MM/YYYY \n")
@@ -64,7 +54,40 @@ def get_lesson_data():
     attendance_data_str = input("Enter your data here: ")
     print("\n")
 
-    print(f"The lesson data provided is: \nDay: {day_data_str}, Date: {date_data_str}, Time: {time_data_str}, Duration: {duration_data_str}, Location: {location_data_str}, Attendance: {attendance_data_str}")
+    print(
+        f"The lesson data provided is: \nDay: {day_data_str}, Date: {date_data_str}, Time: {time_data_str}, Duration: {duration_data_str}, Location: {location_data_str}, Attendance: {attendance_data_str}")
+
+
+# def validate_lesson_data():
+#     """
+#     Valuate the lesson data provided by the user
+#     and returns an error if incorrect
+#     """
+
+#     day = ["monday", "tuesday", "wednesday",
+#            "thursday", "friday", "saturday", "sunday"]
+#     try:
+#         if day =! validate_lesson_data(day_data_str):
+#             print("data is correct")
+#     except ValueError:
+#         print("You have provided incorrect data, please choose a day, example: Monday")
+
+def validate_data(day):
+    """
+    Validate the lesson data provided by the user 
+    and returns an error if incorrect 
+    """
+    lesson_day = [
+        "monday", "tuesday", "wednesday", "thursday", "friday",
+        "saturday", "sunday"]
+
+    try:
+        for day_data_str in lesson_day:
+            if lesson_day != day:
+                print(f"{day_data_str} is incorrect")
+                break
+    except ValueError:
+        print("Incorrect data, please choose a day between Monday - Sunday")
+
 
 get_lesson_data()
-
